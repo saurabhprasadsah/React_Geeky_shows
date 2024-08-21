77f8592d136ec72edf0e389403e1794498f79d99
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-08-21 20:58:57 +0530

    index

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,14 @@ import { LearnEvent } from "../components/LearnEvent";
 import { Learnjsx } from "../components/Learnjsx";
 import { LearnLiftingstateup } from "../components/LearnLiftingstateup";
 import { Learnprops } from "../components/Learnprops";
-
 import { LearnState } from "../components/LearnState";
-
 import { CounterApp } from "../components/counterApp";
 import { LearnUseEffect } from "../components/LearnuseEffect";
-
-
 import { LearnUseMemo } from "../components/LearnUseMemo";
 import { LearnUseCallback } from "../components/LearnUseCallback";
 import { GenrateRandomNumber } from "../components/GenrateRandomNumber";
 
 import { LearnCustomHook } from "../components/LearnCustomHook";
-
-
 
 
 function App() {
--- src/hooks/useCoustomCounter.jsx
+++ src/hooks/useCoustomCounter.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 
+
+
 export const useCoustomCounter = () => {
     const [count, setCount] = useState(0)
     const handleIncrement = () =>{
